ea6e5f9269c67dd8aa3cbc1b6d1a1ee8e120ad4c
MuhammadAnas720 <61290637+MuhammadAnas720@users.noreply.github.com>
2020-08-08 01:58:31 +0500

    a

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const NumOfPlayers = 4;
+const numOfCards = 52;
+var p = [".one", ".two", ".three", ".four"];
+
+
+
 class Card {
   constructor(suit, rank, value, image, team) {
     this.suit = suit;
     this.rank = rank;
     this.value = value;
     this.image = "Cards/" + this.rank + "_of_" + this.suit + ".png";
-    //addImage();
-
   }
 
-  addImage() {
-    // var h = document.querySelector(".player1");
-    // h.insertAdjacentHTML("afterend", "<button><img src='Cards/2_of_clubs.png' class='one 1'></button>");
-
-    var img = document.createElement('img');
-    img.src = "Cards/2_of_clubs.png";
-  }
 }
-
-var h = new Card("clubs", "ace", 14);
 
 class Deck {
   constructor() {
@@ -33,7 +27,6 @@ class Deck {
     for (var i = 0; i < suits.length; i++) {
       for (var j = 0; j < ranks.length; j++) {
         this.cards.push(new Card(suits[i], ranks[j], values[j]));
-        // this.cards.addImage();
       }
     }
   }
@@ -54,15 +47,7 @@ class Player {
   constructor(name) {
     this.playerName = name;
     this.playerCards = [];
-
   }
-  // addClass(){
-  //   for (var i=0; i <5; i++){
-  //     document.querySelector("img").classList.add("one");
-  //       }
-  //   for (var j=0; j <5; j++){
-  //     document.querySelector(".one")[j].setAttribute("src", this.playerCards[j].image);
-  //   }
 
 }
 
@@ -70,6 +55,13 @@ class Board {
   constructor() {
     this.cardsInMiddle = [];
     this.players = [];
+  }
+  namePrompt() {
+    var names = []
+    for (var i = 1; i <= NumOfPlayers; i++) {
+      names.push(prompt("Enter player " + i + " name"));
+    }
+    return names;
   }
   start(playerOneName, playerTwoName, playerThreeName, playerFourName) {
     this.players.push(new Player(playerOneName));
@@ -81,25 +73,82 @@ class Board {
     d.createDeck();
     d.shuffleDeck();
     for (var i = 0; i < NumOfPlayers; i++) {
-      this.players[i].playerCards = d.cards.slice((d.cards.length/NumOfPlayers)*i, (d.cards.length / NumOfPlayers)*(i+1));
+      this.players[i].playerCards = d.cards.slice((d.cards.length / NumOfPlayers) * i, (d.cards.length / NumOfPlayers) * (i + 1));
     }
   }
 
   display() {
-    var p = [".one", ".two", ".three", ".four"];
+
     for (var i = 0; i < NumOfPlayers; i++) {
+      document.querySelectorAll(".playername")[i].innerHTML = this.players[i].playerName + "";
 
       for (var j = 0; j < 13; j++) {
-        //document.querySelectorAll(p[i])[j].setAttribute("src", this.players[i].playerCards[j].image);
-        var s = this.players[i].playerCards[j].image;
-        document.querySelectorAll(p[i])[j].style.backgroundImage = 'url('+s+')';
+        var s = this.players[i].playerCards[j];
+        document.querySelectorAll(p[i])[j].style.backgroundImage = 'url(' + s.image + ')';
+        document.querySelectorAll(p[i])[j].classList.add(s.suit + "-" + s.rank + "-" + s.value);
+        console.log(document.querySelectorAll(p[i])[j]);
       }
+    }
+  }
+  addToBoard(card, team) {
+    if (this.cardsInMiddle.length === 4) {
+      this.cardsInMiddle = [];
+    }
+    var values = card.split("-");
+
+    if ((team == "one") && (this.cardsInMiddle.length ==0)) {
+      this.cardsInMiddle.push(new Card(values[0], values[1], values[2]));
+      return true;
+    }
+    else if ((team == "two") && (this.cardsInMiddle.length ==1)) {
+      this.cardsInMiddle.push(new Card(values[0], values[1], values[2]));
+      return true;
+    }
+    else if ((team == "three") && (this.cardsInMiddle.length ==2)) {
+      this.cardsInMiddle.push(new Card(values[0], values[1], values[2]));
+      return true;
+    }
+    else if ((team == "four") && (this.cardsInMiddle.length ==3)) {
+      this.cardsInMiddle.push(new Card(values[0], values[1], values[2]));
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  }
+  displayBoard() {
+    var s = this.cardsInMiddle.length;
+    console.log(s);
+    for (var i = 0; i < s; i++) {
+      var t = this.cardsInMiddle[i];
+      document.querySelectorAll(".bcard")[i].style.backgroundImage = 'url(' + t.image + ')';
     }
   }
 }
 
 let gameBoard = new Board();
-gameBoard.start('Mario', 'Luigi', 'a', 'b');
+// names = gameBoard.namePrompt();
+// gameBoard.start(names[0], names[1], names[2], names[3]);
+gameBoard.start("a", "b", "c", "d");
 gameBoard.display();
 
-console.log(gameBoard.players);
+var p = [".one", ".two", ".three", ".four"];
+for (var i = 0; i < 52; i++) {
+  // if (i%NumOfPlayers === )
+  var f = p[(i % NumOfPlayers)];
+
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var clicked = this.classList[1];
+    var team = this.classList[0];
+    console.log(team);
+    console.log(clicked);
+    var signal = gameBoard.addToBoard(clicked, team);
+    if (signal == true){
+       this.classList.add("invisible");
+     }
+    gameBoard.displayBoard();
+  });
+}
+
+gameBoard.displayBoard();
